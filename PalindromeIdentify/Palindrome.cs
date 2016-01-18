f9b52d0c09a3768fd344d9d673bceaad0b16689f
Nick Wilson <Nick Wilson>
2016-01-18 14:45:03 -0600

    functional palindrome checker

---
+++ PalindromeIdentify/Palindrome.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeIdentify
+{
+    public class Palindrome
+    {
+        public string getUserInput()
+        {
+            Console.WriteLine("Enter a number to check if it is a palindrome");
+            string userWord = Console.ReadLine();
+            return userWord;
+        }
+
+        public string reverseUserInput(string userWord)
+        {
+            char[] userInputReversed = userWord.ToCharArray();
+            Array.Reverse(userInputReversed);
+            return new string(userInputReversed);
+        }
+
+        public void checkIfPalindrome(string userInputReversed, string userWord)
+        {
+            if (userInputReversed == userWord)
+            {
+                Console.WriteLine("This is, in fact, a palindrome!");
+            }
+            else
+            {
+                Console.WriteLine("This is not a Palindrome");
+            }
+        }
+    }
+}
---
+++ PalindromeIdentify/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeIdentify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Palindrome myPalindrome = new Palindrome();
+            string userInput = myPalindrome.getUserInput();
+            string reverseString = myPalindrome.reverseUserInput(userInput);
+            myPalindrome.checkIfPalindrome(userInput, reverseString);
+        }
+    }
+}
